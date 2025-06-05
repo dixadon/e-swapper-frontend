fe37698f3d81cdeda7841c68585f97a0f550732f
Dixadon <thenixmin@proton.me>
2025-06-05 11:36:54 -0400

    Add files via upload

---
+++ browse.js
@@ -0,0 +1,1 @@
+fetch('https://e-swapper-backend.onrender.com/api/items').then(r => r.json()).then(data => {document.getElementById('items').innerHTML = data.map(item => `<div><h3>${item.title}</h3><p>${item.description}</p></div>`).join('');});
